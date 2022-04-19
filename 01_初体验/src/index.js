3566036d67f4bb3302aa0ff25c86e304061ef074
eat to 160 pounds <2915390277@qq.com>
2022-04-19 19:57:47 +0800

    初体验

---
+++ 01_初体验/src/index.js
@@ -0,0 +1,5 @@
+import {inc, dec} from './js/math.js'
+const {toString} = require('./js/toString.js')
+console.log('加法:', inc(2342, 343))
+console.log('减法:', dec(252, 2352))
+console.log('信息:', toString('张三', 23))
---
+++ 01_初体验/src/js/toString.js
@@ -0,0 +1,6 @@
+const toString = (name, age) => {
+  return `姓名：${name}---年龄：${age}`
+}
+module.exports = {
+  toString
+}
