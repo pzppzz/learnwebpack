71f2b982cc48ed50f53a3fedeac70d7b1f4787fa
eat to 160 pounds <2915390277@qq.com>
2022-04-22 15:16:56 +0800

    更新sourcemap配置

--- 03_webpack配置sourcemap等/webpack.config.js
+++ 03_webpack配置sourcemap等/webpack.config.js
@@ -3,10 +3,13 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'build'),
+    // 配置图片等资源输出路径及文件名 方式一
+    // assetModuleFilename: 'img/[name].[hash:6].[ext]'
   },
   module: {
     rules: [
+      // css
       {
         test: /\.css$/i,
         use: [
@@ -21,6 +24,7 @@ module.exports = {
           'postcss-loader',
         ]
       },
+      // less
       {
         test: /\.less$/i,
         use: [
@@ -40,21 +44,45 @@ module.exports = {
           'postcss-loader',
         ]
       },
+      // 图片
       {
         test: /.(jpg|png|gif|jpeg|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              // 生成文件名
-              name: '[name].[hash:6].[ext]',
-              // 限制，文件大小超过6kb使用http加载，否则转换成base64
-              limit: 6 * 1024,
-              // 输出文件路径
-              outputPath: 'assets'  
-            }
+        // 方式一
+        // use: [
+        //   {
+        //     loader: 'url-loader',
+        //     options: {
+        //       // 生成文件名
+        //       name: '[name].[hash:6].[ext]',
+        //       // 限制，文件大小超过6kb使用http加载，否则转换成base64
+        //       limit: 6 * 1024,
+        //       // 输出文件路径
+        //       outputPath: 'assets'  
+        //     }
+        //   }
+        // ]
+
+        // 方式二 webpack5以后
+        // type: 'asset/resource', // file-loader效果
+        // type: 'asset/inline', // url-loader效果  base64
+        type: 'asset',
+        generator: {
+          filename: 'asset/[name][hash:6][ext]'
+        },
+        parser: {
+          dataUrlCondition: {
+            // 同limit
+            maxSize: 6 * 1024
           }
-        ]
+        }
+      },
+      // 字体
+      {
+        test: /\.(woof2?|ttf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'font/[name]-[hash:6][ext]'
+        }
       }
     ]
   }
