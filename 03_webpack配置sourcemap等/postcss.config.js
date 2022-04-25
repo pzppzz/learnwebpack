31a4b42da0ef388286853f34321f8ae43c22e7a9
eat to 160 pounds <2915390277@qq.com>
2022-04-25 13:14:15 +0800

    更改文件夹名字

--- 03_webpack配置sourcemap等/postcss.config.js
+++
@@ -1,6 +0,0 @@
-/** 单独配置postcss 避免在webpack.config.js中进行重复操作 */
-module.exports = {
-  plugins: [
-    'postcss-preset-env'
-  ]
-}
--- 03_webpack配置sourcemap等/src/index.js
+++
@@ -1,7 +0,0 @@
-import create from "./js/create.js";
-import imgSrc from './img/OIP-C (7).jpg'
-const div = create('div')
-const img = new Image(300)
-img.src = imgSrc
-document.body.appendChild(div)
-document.body.appendChild(img)
