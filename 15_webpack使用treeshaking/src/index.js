f9c4c2f7c966c245694ae346f78d63bc80afbbd0
eat to 160 pounds <2915390277@qq.com>
2022-05-10 19:39:04 +0800

    webpack 配置treeshaking

---
+++ 15_webpack使用treeshaking/src/index.js
@@ -0,0 +1,4 @@
+import {inc, dec} from './js/math'
+import _ from 'lodash'
+console.log(inc(1, 1))
+// console.log(dec(1, 1))
---
+++ 15_webpack使用treeshaking/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    usedExports: true
+  }
+}
