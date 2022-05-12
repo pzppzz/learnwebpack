826f33caa03c3a5c4e8e657a28270d44d337691b
eat to 160 pounds <2915390277@qq.com>
2022-05-12 14:37:44 +0800

    js css treeshaking

--- 15_webpack使用treeshaking/src/index.js
+++ 15_webpack使用treeshaking/src/index.js
@@ -1,4 +1,10 @@
 import {inc, dec} from './js/math'
-import _ from 'lodash'
+import 'lodash'
+import './index.css'
 console.log(inc(1, 1))
 // console.log(dec(1, 1))
+// console.log(window._)
+// const title = document.createElement('h1')
+// title.className = 'title'
+// title.innerText = 'title'
+// document.body.appendChild(title)
--- 15_webpack使用treeshaking/webpack.config.js
+++ 15_webpack使用treeshaking/webpack.config.js
@@ -1,4 +1,14 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const PurgeCssWebpackPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob')
+const paths = {
+  src: path.join(__dirname, 'src')
+}
+console.log(paths.src)
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
@@ -9,5 +19,26 @@ module.exports = {
   },
   optimization: {
     usedExports: true
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css'
+    }),
+    new CssMinimizerPlugin(),
+    new PurgeCssWebpackPlugin({
+      paths: glob.sync(`${paths.src}/**/*`, {nodir: true})
+    })
+  ]
 }
